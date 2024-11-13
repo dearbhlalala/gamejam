88dfc688304afe92ff00b7b570ace4848315162e
Noah <noahbob34@gmail.com>
2024-11-13 01:06:59 +0000

    added basic movement code and ground

--- SpaceJam/Assets/playermovement.cs
+++ SpaceJam/Assets/playermovement.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private Rigidbody2D body;
         
-    }
+        private void Awake()
+        {
+                body = GetComponent<Rigidbody2D>();
+        }
+        
+        private void Update()
+        {
+                body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        }
+        
+        
 }
