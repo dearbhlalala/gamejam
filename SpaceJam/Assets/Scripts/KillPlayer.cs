d1e6602195adb094f154505f6c6b8c7509a9e137
amy <C24399391@myTUDublin.ie>
2024-11-14 23:34:18 +0000

    death and respawn test finito

---
+++ SpaceJam/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
