5f9a94ea051e93d1ad5b93fb55117faa13fc3022
Noah <noahbob34@gmail.com>
2024-11-17 02:24:15 +0000

    all sound effects added

--- SpaceJam/Assets/Scripts/OrbInteract.cs
+++ SpaceJam/Assets/Scripts/OrbInteract.cs
@@ -6,11 +6,13 @@ public class OrbAndDoorDeactivation : MonoBehaviour
     public GameObject door; 
     public TextMeshProUGUI messageText;
     public MessageTextBehavior messageBehavior;
+    [SerializeField] private AudioClip orbSoundClip;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(orbSoundClip, transform.position, 1f);
             gameObject.SetActive(false);
             door?.SetActive(false); 
             
