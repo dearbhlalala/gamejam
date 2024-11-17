332118f02a0c7d632635eca65d81feaa3877731d
Dearbhla <dearbhlalane@gmail.com>
2024-11-17 17:03:38 +0000

    finishing up

--- SpaceJam/Assets/EndLevel.cs
+++ SpaceJam/Assets/EndLevel.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextLevel : MonoBehaviour
+public class EndLevel : MonoBehaviour
 {
-  private void OnTriggerEnter2D(Collider2D other)
+   private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("TJScene Level 1", LoadSceneMode.Single);
+            SceneManager.LoadScene("ENDINGTEST", LoadSceneMode.Single);
         }
     }
 }
